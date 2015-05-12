954683afaf4741ab7e44b5da32c80c0681258408
sorin <sorin.nica@woowteam.com>
2015-05-12 20:23:54 +0300

    added the decorator class

---
+++ src/main/java/fractal/crud_rest_json/persistence/Decorator.java
@@ -0,0 +1,22 @@
+package fractal.crud_rest_json.persistence;
+
+import java.util.Date;
+
+public class Decorator<T extends Identifiable<K>, K> {
+
+	private final KeyGenerator<K>	keyGenerator;
+
+	public Decorator(KeyGenerator<K> keyGenerator) {
+		this.keyGenerator = keyGenerator;
+	}
+
+	public void decorate(T t) {
+		Date now = new Date();
+		if (!t.getId().isPresent()) {
+			t.setId(keyGenerator.generate());
+			t.setCreated(now);
+		}
+		t.setLastUpdated(now);
+	}
+
+}
